eac6a2ebb69e8d49f339ab120cdd1c662310ae5a
Harald Brunner <brunnerh@users.noreply.github.com>
2020-08-27 00:10:33 +0200

    Init.

---
+++ src/app.svelte
@@ -0,0 +1,5 @@
+<script type="text/typescript">
+	import Input from "./components/input.svelte";
+</script>
+
+<Input label="Input" on:input={() => console.log('!')}/>
---
+++ src/components/input.svelte
@@ -0,0 +1,24 @@
+<script type="text/typescript">
+	export let id: string | undefined = undefined;
+	export let value: any = undefined;
+	export let required: boolean = false;
+	export let label: string;
+
+	export let trailingIcon: boolean | undefined = false;
+
+	$: resolvedId = id ?? ('id-' + Math.random().toString().replace('0.', ''));
+</script>
+
+<div class="root">
+	<input id={resolvedId} on:input bind:value/>
+
+	<label for={resolvedId} class:required={required}>
+		{label}
+	</label>
+
+	{#if trailingIcon}
+		<div class="trailing-icon-container">
+			<slot name="trailing-icon"/>
+		</div>
+	{/if}
+</div>
